d28364878ab9efa28e4522406f890454bff776cc
Uzytkownik <Uzytkownik@kursGIT.pl>
2020-09-19 14:26:32 +0200

    Added index.html and HomeController

---
+++ src/main/java/com/sda/meetup/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.sda.meetup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+class HomeController {
+
+    @RequestMapping
+    ModelAndView indexPage() {
+        ModelAndView mnv = new ModelAndView("index");
+       return mnv;
+    }
+
+}
+
