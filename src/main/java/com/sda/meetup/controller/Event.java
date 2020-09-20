bd5e836173ac7a4a803dc8a65c3c4f55273356e6
Magdalena Borettini <magdalena.borettini@gmail.com>
2020-09-20 15:02:50 +0200

    Personal calendar page demo

---
+++ src/main/java/com/sda/meetup/controller/Event.java
@@ -0,0 +1,16 @@
+package com.sda.meetup.controller;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Event {
+
+    private String title;
+    private String description;
+    private String time;
+
+}
